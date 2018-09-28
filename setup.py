1e1d39f6166fc3f5f3426da8ed2a4d253ebb22d9
Michael <tuckerma@appstate.edu>
2018-09-28 10:03:24 -1000

    edits to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ PACKAGES = [
 	'argparse'
 ]
 
+FileList = [
+	'quick-class.py',
+	'obstools.py',
+	'extractSNIFS.py'
+]
+
 
 def main():
 	cwd = os.getcwd()
@@ -17,7 +23,13 @@ def main():
 	for pkg in PACKAGES:
 		CheckPkg(pkg)
 
-	resp = input('Make Python scripts executables? [Y/n] >')
+	resp = input('Make Python scripts executables? [Y/n] >').strip().lower()
+	if resp == 'y':
+		MakeExecutables()
+
+	resp2 = input('Add this directory to $PATH? [Y/n] >').strip().lower()
+	if resp == 'y':
+		AddToPath()
 
 
 def CheckPkg(pkg):
@@ -30,6 +42,35 @@ def CheckPkg(pkg):
 		exec('import %s' % pkg)
 		return
 
+def MakeExecutables():
+	python_cmd = shutil.which('python')
+	startline = '#!'+python_cmd+'\n'
+	for ff in FileList:
+		lines = open(ff, 'r').readlines()
+		if lines[0].startswith('#!'): continue
+		lines = [startline]+lines
+		with open(ff, 'w') as ofile:
+			ofile.write(''.join(lines))
+
+		subprocess.run('chmod u+x '+ff)
+
+
+def AddToPath():
+	bashrc = '~/.bashrc'
+	if not os.path.isfile(bashrc): 
+		print('WARNING (AddToPath): Could not find ~/.bashrc file, cannot add to path!')
+		return
+
+	cwd = os.getcwd()
+	PATHline = 'export PATH="%s:$PATH"\n' % cwd
+	for line in open(bashrc,'r').readlines():
+		if line == PATHline: return
+
+	with open(bashrc, 'a+') as ofile:
+		ofile.write(PATHline)
+
+	subprocess.run('source '+bashrc)
+	print('Successfully added to $PATH!')
 
 if __name__=='__main__':
 	main()
